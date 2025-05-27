925198efbbd16d0f9752ead4d3038dfc2d418883
amran1451 <aeamarda@gmail.com>
2025-05-27 13:46:24 +0300

    feat: session_provider

--- lib/src/providers/session_provider.dart
+++ lib/src/providers/session_provider.dart
@@ -1,12 +1,26 @@
+// lib/src/providers/session_provider.dart
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import '../data/session_repository.dart';
-import '../models/session_entry.dart'; 
-import '../models/workout_session.dart';
+import '../data/cloud_session_repository.dart';
+import '../models/session_entry.dart';
 
-final sessionRepoProvider = Provider<SessionRepository>((ref) => SessionRepository());
+/// Провайдер локального SQLite-репозитория
+final sessionRepoProvider =
+    Provider<SessionRepository>((ref) => SessionRepository());
 
-final historyProvider = FutureProvider.autoDispose<List<WorkoutSession>>(
-  (ref) => ref.read(sessionRepoProvider).getAll(),
-);
+/// Провайдер облачного Firestore-репозитория
+final cloudSessionRepoProvider =
+    Provider<CloudSessionRepository>((ref) {
+  final uid = FirebaseAuth.instance.currentUser?.uid;
+  if (uid == null) {
+    throw Exception('User not signed in');
+  }
+  return CloudSessionRepository(uid);
+});
 
-final sessionEntriesProvider = StateProvider<List<SessionEntry>>((ref) => []);
+/// Провайдер для временных записей на экране (UI state)
+final sessionEntriesProvider =
+    StateProvider<List<SessionEntry>>((ref) => []);
