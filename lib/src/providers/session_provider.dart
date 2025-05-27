164e3a67df129c1b299e71b255a41a2c432800b4
amran1451 <aeamarda@gmail.com>
2025-05-27 12:46:49 +0300

    fix: import SessionEntry and SessionRepository in session_provider.dart

--- lib/src/providers/session_provider.dart
+++ lib/src/providers/session_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../data/repository.dart';
+import '../data/session_repository.dart';
+import '../models/session_entry.dart'; 
 import '../models/workout_session.dart';
 
 final sessionRepoProvider = Provider<SessionRepository>((ref) => SessionRepository());
